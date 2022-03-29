8ac995e0f1fd90efd637b270d35175a20e7e0465
友岡湖太 <sirogamicota@gmail.com>
2022-03-30 02:04:06 +0900

    Update script.js

--- skipper/script.js
+++ skipper/script.js
@@ -2,15 +2,28 @@ const c = 'a'.charCodeAt(0);
 const alphabets = Array.apply(null, new Array(26)).map((v, i) => {
     return String.fromCharCode(c + i);
 });
-const nums = [...Array(100).keys()]
+alphabets.push('')
+const nums = [];
+for (let i = 0; i < 10; i++) {
+    nums.push(String(i))
+}
 const identifier = alphabets.concat(nums)
+console.log(identifier)
 
 setInterval(() => {
-    identifier.forEach(function (c) {
-        const selector = '#skip-button\\:' + c +' > span > button'
-        const button = document.querySelector(selector);
-        if (button != null) {
-            button.click();
-        }
+    identifier.forEach(function (c1) {
+        identifier.forEach(function (c2) {
+            const s1 = '#skip-button\\:' + c1 + c2 +' > span > button'
+            const b1 = document.querySelector(s1);
+            if (b1 != null) {
+                b1.click();
+            }
+
+            const s2 = '#preskip-component\:' + c1 + c2 + ' > span'
+            const b2 = document.querySelector(s2)
+            if (b2 != null) {
+                b2.click();
+            }
+        })
     })
 }, 1000)
