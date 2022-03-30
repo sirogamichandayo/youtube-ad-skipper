056e61ad21f806d5c289b24c6a3f04a84b48a75f
友岡湖太 <sirogamicota@gmail.com>
2022-03-30 13:15:38 +0900

    Update script.js

--- skipper/script.js
+++ skipper/script.js
@@ -10,19 +10,20 @@ for (let i = 0; i < 10; i++) {
 const identifier = alphabets.concat(nums)
 
 setInterval(() => {
+    console.log('hoge');
     identifier.forEach(function (c1) {
         identifier.forEach(function (c2) {
-            const s1 = '#skip-button\\:' + c1 + c2  +' > span > button'
+            const s1 = `#skip-button\\:${c1}${c2} > span > button`
             const b1 = document.querySelector(s1);
             if (b1 != null) {
                 b1.click();
             }
 
-            const s2 = '#preskip-component\\:' + c1 + c2 + ' > span'
+            const s2 = `#preskip-component\\:${c1}${c2} > span`
             const b2 = document.querySelector(s2)
             if (b2 != null) {
                 b2.click();
             }
         })
     })
-}, 1000)
+}, 100) // 0.1s
